f18b8d18cd956506b871b6f3956c78729c5b126d
smolenski-mikolaj <31181850+smolenski-mikolaj@users.noreply.github.com>
2017-08-27 11:49:45 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,3 @@
 var name = prompt('Enter your name');
-alert('Hello, ' + name); // Wiadomość wyświetlana w oknie zawierająca zmienną 'name'
+alert('Hello, ' + name);
 console.log('Hello, ' + name);
-/*
-	Powyższa funkcja wyświetla w konsoli
-	tekst - "Hello" oraz imię użytkownika
-	pobrane za pomocą 'prompt'
-*/
