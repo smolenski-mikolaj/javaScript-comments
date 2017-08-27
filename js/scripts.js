582fed8b52379d16c8162a6b0f9a9dba26ca3eac
smolenski-mikolaj <31181850+smolenski-mikolaj@users.noreply.github.com>
2017-08-27 11:53:23 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
 var name = prompt('Enter your name');
-alert('Hello, ' + name);
-console.log('Hello, ' + name);
+alert('Hello, ' + name); // Here is the message with username
+console.log('Hello, ' + name); 
+
+/*
+	The function which is above
+	shows in console message:
+	"Hello, |username|"
+*/
